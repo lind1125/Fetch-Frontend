d197357e54803b5ee71ec199f45acdcdb41559d7
Monica <monica.parrillo@gmail.com>
2021-01-02 14:04:51 -0600

    handle scenario where dogdata not passed on state

--- src/components/DogProfile.js
+++ src/components/DogProfile.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
+import {Link} from 'react-router-dom'
+
 // destructure props.location.state to have access on state
 const DogProfile = ({location:{state}}) => {
+  // TODO update to handle if the dog page accessed from elsewhere - i.e. if not props.location.state then do an axios request and render that.
+  const [dogdata, setDogdata] = useState(state)
+  useEffect(()=>{
+    if(!state){
+      console.log('no dog data')
+      //make axios request to set dog data TODO
+    }
+  },[])
 
-  return (
+  const display = () => {
+    if(dogdata){
+      return(
+        <div><h1>Dog profile page {dogdata.dog.name}</h1>
+        <p>Breed: {dogdata.dog.breed}</p>
+        <img alt="Your dog" src={dogdata.dog.picture_url}/></div>
+      )
+    } else {
+      return <p>No dog <Link to={"/profile"} className="nav-link">return home</Link></p>
+    }
+  }
 
-  <div><h1>Dog profile page {state.dog.name}</h1>
-  <p>Breed: {state.dog.breed}</p>
-  <img alt="Your dog" src={state.dog.picture_url}/></div>
-)};
+  return display()
+};
 
 export default DogProfile;
