4760191c98b30f7a27c9bddde3d3fc63bf89d770
lind1125 <danieljlinden@gmail.com>
2021-01-07 22:24:54 -0600

    imported common components for formgroup and btnspinner

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import Form from 'react-validation/build/form'
 import Input from 'react-validation/build/input'
+// Common componenets
 import FormGroup from './common/FormGroup'
+import BtnSpinner from './common/BtnSpinner'
 
 
 const ImageUpload = () => {
 
-  
+  const [image, setImage] = useState('');
+  const [loading, setLoading] = useState(false)
+
 
   return (
     <div>
@@ -21,6 +26,7 @@ const ImageUpload = () => {
             // validations={[required]}
             />
         </FormGroup>
+        <BtnSpinner loading={loading} text="Submit"/>
       </Form>
     </div>
   )
