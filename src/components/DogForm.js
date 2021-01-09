c954db07db0e388940b8b8ad945fc27967d6ace5
Monica <monica.parrillo@gmail.com>
2021-01-08 21:39:04 -0600

    First few components of new dog form + validator

--- src/components/DogForm.js
+++ src/components/DogForm.js
@@ -12,8 +12,56 @@ import {resMessage} from '../utils/functions.utils'
 import {getCurrentUser} from '../services/auth.service'
 import {newUserDog} from '../services/dogs.service.js'
 
+const required = (value) => {
+  if(!value){
+    return (
+      <div className="alert alert-danger" role="alert">
+        This field is required!
+      </div>
+    )
+  }
+}
+
+const vurl = (value) => {
+  const img_regex = /(\.jpg)$|(\.png)$|(\.jpeg$)/
+  if(!value.match(img_regex)){
+    return (
+      <div className="alert alert-danger" role="alert">
+        Must be a valid image (jpeg, jpg, or png)
+      </div>
+    )
+  }
+}
+
+const textLengthBio = (value) => {
+  if(value.length>500){
+    return (
+      <div className="alert alert-danger" role="alert">
+        Must be less than 500 characters
+      </div>
+    )
+  }
+}
+
+const textLengthTemp = (value) => {
+  if(value.length>25){
+    return (
+      <div className="alert alert-danger" role="alert">
+        One or two words will suffice
+      </div>
+    )
+  }
+}
+
 
 const DogForm = (props) => {
+  const form = useRef()
+  const checkBtn = useRef()
+
+
+  const [message,setMessage] = useState("")
+  const [loading,setLoading] = useState(false)
+  const [successful, setSuccessful] = useState(false)
   const [data,setData] = useState(
     {
       name:"",
@@ -34,13 +82,106 @@ const DogForm = (props) => {
     setData({...data,[e.target.name]:e.target.value})
   }
 
-
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // use the library to validate all fields on the form
+    form.current.validateAll()
+    // check on ages and sizes
+    if(data.min_age>data.max_age){
+      setMessage("Min age must be less than max age")
+      return
+    }
+    if(data.min_size>data.max_size){
+      setMessage("Min size must be less than max size")
+      return
+    }
+    setLoading(true)
+    // check min_age lte max_age and same for size
+    newUserDog({data})
+    // .then((err,res)=>{
+    //
+    // })
+    setLoading(false)
+  }
 
   const display = () => {
     return !getCurrentUser() ?
     <NotLoggedIn/>
     : (
-      <div></div>
+      <div className="col-md-12">
+        <div className="card card-container">
+          <Form onSubmit={handleSubmit} ref={form}>
+
+            <FormGroup text="Dog's Name">
+              <Input
+                type="text"
+                className="form-control"
+                name="name"
+                value={data.name}
+                onChange={handleChange}
+                validations={[required]}
+              />
+            </FormGroup>
+
+            <FormGroup text='Link to Picture'>
+              <Input
+                type="text"
+                className="form-control"
+                name="picture_url"
+                value={data.picture_url}
+                onChange={handleChange}
+                validations={[required,vurl]}
+              />
+            </FormGroup>
+
+            <FormGroup text="Short Bio">
+              <Input
+                type="text"
+                className="form-control"
+                name="biography"
+                value={data.biography}
+                onChange={handleChange}
+                validations={[required,textLengthBio]}
+              />
+            </FormGroup>
+
+            <FormGroup text="Temperament">
+              <Input
+                type="text"
+                className="form-control"
+                name="temperament"
+                value={data.temperament}
+                onChange={handleChange}
+                validations={[required,textLengthTemp]}
+              />
+            </FormGroup>
+
+            <FormGroup text="Age">
+              <Input
+                type="number"
+                className="form-control"
+                name="age"
+                value={data.age}
+                onChange={handleChange}
+                validations={[required]}
+              />
+            </FormGroup>
+
+
+
+            <BtnSpinner loading={loading} text="Create Dog"/>
+
+           {message && (
+               <div className='form-group'>
+                   <div className={successful ? 'alert alert-success':'alert alert-danger'} role='alert'>
+                       {message}
+                   </div>
+               </div>
+           )}
+           <CheckButton style={{display:'none'}} ref={checkBtn} />
+          </Form>
+        </div>
+      </div>
     )
   }
   return display()
