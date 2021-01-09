f5135053d33ab57fa619f9b80d632e9945873f10
Monica <monica.parrillo@gmail.com>
2021-01-08 22:21:17 -0600

    Add all fields to new dog form

--- src/components/DogForm.js
+++ src/components/DogForm.js
@@ -2,7 +2,8 @@ import React, {useState, useRef} from 'react';
 import Form from 'react-validation/build/form'
 import Input from 'react-validation/build/input'
 import CheckButton from 'react-validation/build/button'
-import validator from 'validator';
+import Select from 'react-validation/build/select'
+
 // Common components we made
 import FormGroup from './common/FormGroup'
 import BtnSpinner from './common/BtnSpinner'
@@ -70,11 +71,11 @@ const DogForm = (props) => {
       breed:"",
       temperament:"",
       age:"",
-      size:"",
+      size:1,
       min_age:"",
       max_age:"",
-      min_size:"",
-      max_size:"",
+      min_size:1,
+      max_size:3,
       }
     )
 
@@ -84,6 +85,7 @@ const DogForm = (props) => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    setMessage("")
     // use the library to validate all fields on the form
     form.current.validateAll()
     // check on ages and sizes
@@ -109,7 +111,7 @@ const DogForm = (props) => {
     <NotLoggedIn/>
     : (
       <div className="col-md-12">
-        <div className="card card-container">
+        <div className="card">
           <Form onSubmit={handleSubmit} ref={form}>
 
             <FormGroup text="Dog's Name">
@@ -156,6 +158,17 @@ const DogForm = (props) => {
               />
             </FormGroup>
 
+            <FormGroup text="Breed">
+              <Input
+                type="text"
+                className="form-control"
+                name="breed"
+                value={data.breed}
+                onChange={handleChange}
+                validations={[required,textLengthTemp]}
+              />
+            </FormGroup>
+
             <FormGroup text="Age">
               <Input
                 type="number"
@@ -166,6 +179,76 @@ const DogForm = (props) => {
                 validations={[required]}
               />
             </FormGroup>
+
+
+            <FormGroup text='Size'>
+              <Select
+                name='size'
+                className="form-control"
+                value={data.size}
+                onChange={handleChange}
+                validations={[required]}>
+                <option value='1'>Small</option>
+                <option value='2'>Medium</option>
+                <option value='3'>Large</option>
+              </Select>
+            </FormGroup>
+
+            {/* Preferences */}
+            <h3>Preferences</h3>
+            <div className="form-row">
+              <div className="form-group col-md-3">
+                <label htmlFor="min_age">Min Age</label>
+                <Input
+                  type="number"
+                  className="form-control"
+                  name="min_age"
+                  value={data.min_age}
+                  onChange={handleChange}
+                  validations={[required]}
+                />
+              </div>
+              <div className="form-group col-md-3">
+                <label htmlFor="inputState">Max Age</label>
+                <Input
+                  type="number"
+                  className="form-control"
+                  name="max_age"
+                  value={data.max_age}
+                  onChange={handleChange}
+                  validations={[required]}
+                />
+              </div>
+
+              <div className="form-group col-md-3">
+                <label htmlFor="inputState">Min Size</label>
+                <Select
+                  className="form-control"
+                  name="min_size"
+                  value={data.min_size}
+                  onChange={handleChange}
+                  validations={[required]}>
+                  <option value='1'>Small</option>
+                  <option value='2'>Medium</option>
+                  <option value='3'>Large</option>
+                </Select>
+              </div>
+
+              <div className="form-group col-md-3">
+                <label htmlFor="inputState">Max Size</label>
+                <Select
+                  className="form-control"
+                  name="max_size"
+                  value={data.max_size}
+                  onChange={handleChange}
+                  validations={[required]}>
+                  <option value='1'>Small</option>
+                  <option value='2'>Medium</option>
+                  <option value='3'>Large</option>
+                </Select>
+              </div>
+            </div>
+
 
 
 
