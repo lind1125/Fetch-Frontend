9866b6fbb992bc78c6186916ade9fbbbd0eb50ee
Monica <monica.parrillo@gmail.com>
2021-01-08 20:19:12 -0600

    stub out state for the form

--- src/components/DogForm.js
+++ src/components/DogForm.js
@@ -1,9 +1,49 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
+import Form from 'react-validation/build/form'
+import Input from 'react-validation/build/input'
+import CheckButton from 'react-validation/build/button'
+import validator from 'validator';
+// Common components we made
+import FormGroup from './common/FormGroup'
+import BtnSpinner from './common/BtnSpinner'
+import NotLoggedIn from './common/NotLoggedIn'
+// helpers
+import {resMessage} from '../utils/functions.utils'
+import {getCurrentUser} from '../services/auth.service'
+import {newUserDog} from '../services/dogs.service.js'
+
 
 const DogForm = (props) => {
-  const [data,setData] = useState({username:"",password:"",email:""})
-  return (
-  <h1>New Dog Form</h1>
-);}
+  const [data,setData] = useState(
+    {
+      name:"",
+      picture_url:"",
+      biography:"",
+      breed:"",
+      temperament:"",
+      age:"",
+      size:"",
+      min_age:"",
+      max_age:"",
+      min_size:"",
+      max_size:"",
+      }
+    )
+
+  const handleChange = (e) =>{
+    setData({...data,[e.target.name]:e.target.value})
+  }
+
+
+
+  const display = () => {
+    return !getCurrentUser() ?
+    <NotLoggedIn/>
+    : (
+      <div></div>
+    )
+  }
+  return display()
+}
 
 export default DogForm;
