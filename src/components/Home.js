76c6fe6f33983c9c761413c2c3c30694b8aad7d2
lind1125 <danieljlinden@gmail.com>
2021-01-30 12:17:03 -0600

    updated display elements on home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,14 @@ import {Link} from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div className='jumbotron'>
-      <h1 className='display-5'>Welcome to Fetch!</h1>
-      <h3 className='display-6'>Find Your Best Friend's New Best Friend</h3>
-      <Link to={'/login'}  className="btn btn-primary">Log In</Link>
-      <Link to={'/signup'} className="btn btn-primary">Sign Up</Link>
-    </div>
+      <div className='jumbotron d-flex flex-column align-items-center w-50 px-2 mx-auto'>
+        <h1 className='display-5'>Welcome to Fetch!</h1>
+        <h3 className='display-6'>Find Your Best Friend's New Best Friend</h3>
+        <div class="d-flex justify-content-around px-2">
+        <Link to={'/login'}  className="btn btn-primary mx-3">Log In</Link>
+        <Link to={'/signup'} className="btn btn-primary mx-3">Sign Up</Link>
+        </div>
+      </div>
   )
 };
 
